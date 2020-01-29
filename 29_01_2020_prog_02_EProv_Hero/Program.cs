89a53c65ba98e621886e10adaf56eca2ab63ebde
BeepBoopError <emil0131@gmail.com>
2020-01-29 14:05:09 +0100

    Add project files.

---
+++ 29_01_2020_prog_02_EProv_Hero/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_01_2020_prog_02_EProv_Hero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
